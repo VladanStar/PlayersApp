977a2dfbe2f59c72dba014bc84afb7b3e30d8dc9
VladanStar <vladancupric@gmail.com>
2023-03-25 18:21:07 +0100

    Add

--- src/app/components/add-player/add-player.component.ts
+++ src/app/components/add-player/add-player.component.ts
@@ -6,7 +6,7 @@ import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
   selector: 'app-add-player',
-  templateUrl: './add-player-component.html',
+  templateUrl: './add-player.component.html',
   styleUrls: ['./add-player.component.css']
 })
 export class AddPlayerComponent implements OnInit {
